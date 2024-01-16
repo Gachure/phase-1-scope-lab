ad0e113a4f8219c89a98617da70ba04bcf824718
Gachure <your.email@example.com>
2024-01-16 08:04:38 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'new best customer';
+  }
+  const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // Trying to change a constant will result in an error
+  leastFavoriteCustomer = 'new least favorite';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
